b1d77df13ef239f94350251c255eb47e794f3774
James Zhang <jameszhanga@gmail.com>
2022-02-11 02:50:18 -0600

    add assignment tests

--- assignments/03/interval.py
+++ assignments/03/interval.py
@@ -5,11 +5,25 @@ for _ in range(int(input())):
     # number of total intervals
     num_inter = int(input())
 
-    S = []
-    # turn input into dict of lists
-    for _ in range(num_inter):
-        interval = input().split()
-        S.append(interval)
+    # edge case
+    if num_inter < 1:
+        res.append(0)
+        continue
 
-    order = sorted(S, key=lambda [a, b]: b)
-    print(order)
+    # turn input into lists of lists
+    S = [input().split() for _ in range(num_inter)]
+
+    # Sort based on end time
+    intervals = sorted(S, key=lambda a: a[1])
+
+    jobs = 1
+
+    for i in range(1, len(intervals)):
+        if intervals[i][0] >= intervals[i - 1][1]:
+            jobs += 1
+
+    res.append(f"{jobs}")
+
+
+for n in res:
+    print(n)
