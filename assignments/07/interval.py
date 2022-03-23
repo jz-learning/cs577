e816ceaeb6d0e9a617d0ab8bb08c2fd807faa0be
James Zhang <jameszhanga@gmail.com>
2022-03-22 23:41:02 -0500

    Implement binary search to speed things up

--- assignments/07/interval.py
+++ assignments/07/interval.py
@@ -1,16 +1,30 @@
-def find_i(jobs: list[tuple], job: tuple, n: int) -> int:
+def find_i_bs(endtimes, target):
     """
-    finds the index with the largest end time < job[start time]
+    BINARY SEARCH: finds the index with the largest end time < job[start time]
     """
-    start = job[0]
-    for i in range(n - 1, -1, -1):
-        if jobs[i][1] <= start:
-            # need + 1 since dp has an extra blank space in the begining
-            return i + 1
+    l, r = 0, len(endtimes) - 1
+    mid = 0
+
+    while l <= r:
+        # edge case to save some time
+        if target >= endtimes[r]:
+            return r + 1
+
+        mid = (l + r) // 2
+        mid_num = endtimes[mid]
+
+        if target < mid_num:
+            if target >= endtimes[mid - 1]:
+                return mid
+            r = mid - 1
+
+        elif target >= mid_num:
+            l = mid + 1
+
     return 0
 
 
-def interval(jobs, n):
+def interval(jobs, endtime, n):
     # zero index is empty by default
     dp = [0] + [0] * n
 
@@ -18,12 +32,10 @@ def interval(jobs, n):
         j = i + 1
 
         # finds closest available request
-        prev = find_i(jobs, job, i)
+        prev = find_i_bs(endtime[:i], job[0])
 
-        count = dp[prev] + job[2]
-        no_count = dp[i]
-        # print(i, job, count, no_count)
-        dp[j] = max(count, no_count)
+        # max of adding a previous job or not adding
+        dp[j] = max(dp[prev] + job[2], dp[i])
 
     return dp[j]
 
@@ -36,18 +48,19 @@ def main():
     for _ in range(instance):
         n = int(input())  # num of jobs
         jobs = []
+        endtime = []
 
         # sanitize input
         for _ in range(n):
             # (start time, finish time, value)
-            request = tuple(map(int, input().split()))
-            jobs.append(request)
+            jobs.append(tuple(map(int, input().split())))
 
         # sort jobs by increasing end time
         jobs = sorted(jobs, key=lambda job: job[1])
+        endtime = [job[1] for job in jobs]
 
         # run algo
-        value = interval(jobs, n)
+        value = interval(jobs, endtime, n)
         res += f"{value}\n"
 
     # Get rid of trailing newline char
