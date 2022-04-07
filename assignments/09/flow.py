419da63ec43b61125cbc54937d75592245a624d4
James Zhang <jameszhanga@gmail.com>
2022-04-06 23:34:23 -0500

    Initialize assignment 9

---
+++ assignments/09/flow.py
@@ -0,0 +1,27 @@
+def flow():
+    pass
+
+
+def main():
+    # number of test cases
+    instance = int(input())
+    res = ""
+
+    for _ in range(instance):
+        items = []
+        # num nodes and edges
+        n, e = map(int, input().split())
+
+        for _ in range(e):
+            src, dst, cap = map(int, input().split())
+            print(src, dst, cap)
+
+        ans = flow()
+        res += f"{ans}\n"
+
+    # Get rid of trailing newline char
+    print(res.rstrip())
+
+
+if __name__ == "__main__":
+    main()
